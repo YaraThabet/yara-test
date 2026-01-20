455c74d9df3dc404427f98c63cfd55863cffa5aa
yara <yarathabet66@gmail.com>
2026-01-20 10:20:43 +0200

    delete navigate

--- src/modules/Products/views/index.tsx
+++ src/modules/Products/views/index.tsx
@@ -68,7 +68,7 @@ export const Products = () => {
        {productWithDiscountLowerThan10.map((product) => {
         return (
       
-          <Grid.Col span={4} key={product.id}  onClick={() => navigate({ to:"/ptoduct/$productId", params:{}})}>
+          <Grid.Col span={4} key={product.id} >
             <Card shadow="sm" padding="lg" radius="md" withBorder>
               <Card.Section>
                 <Image src={product.image} height={160} alt="yara" />
